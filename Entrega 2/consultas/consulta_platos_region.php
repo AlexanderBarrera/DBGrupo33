98c5a4e1ed7e2ea26babdaeefcd2197b7d5531bb
vaetchegaray <vaetchegaray@uc.cl>
2019-04-22 20:42:56 -0400

    all tables done

--- Entrega 2/consultas/consulta_platos_region.php
+++ Entrega 2/consultas/consulta_platos_region.php
@@ -6,10 +6,10 @@
   require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
 
   $var = $_POST["nombre_region"];
-  echo "<h1> Restaurantes en la region $var </h1>";
+  echo "<h1> Platos en la region $var </h1>";
   // Para no tener problemas con los '
   $var = str_replace("'", "''", $var);
-  $query = "SELECT RA.nombre, RE.nombre FROM Restaurantes RE, Regiones RA WHERE RA.nombre='$var' AND RE.id_region = RA.id_region;";
+  $query = "SELECT RE.nombre, PLA.nombre FROM restaurantes RE, regiones RA, restaurantes_regiones RE_RA, platos PLA, restaurantes_platos RE_PLA  WHERE RA.nombre='$var' AND RE.id_restaurante = RE_RA.id_restaurante AND RA.id_region = RE_RA.id_region AND RE.id_restaurante = RE_PLA.id_restaurante;";
   $result = $db -> prepare($query);
   $result -> execute();
   $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
@@ -17,8 +17,8 @@
 
   <table>
     <tr>
-      <th>Region</th>
-      <th>Nombre</th>
+      <th>Restaurante</th>
+      <th>Plato</th>
     </tr>
   <?php
   foreach ($dataCollected as $p) {

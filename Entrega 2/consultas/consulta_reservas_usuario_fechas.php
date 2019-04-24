7c99890817a71054b17aa3f070284c9f915c2092
vaetchegaray <vaetchegaray@uc.cl>
2019-04-24 15:45:07 -0400

    dates fix

--- Entrega 2/consultas/consulta_reservas_usuario_fechas.php
+++ Entrega 2/consultas/consulta_reservas_usuario_fechas.php
@@ -11,9 +11,9 @@
   $f_i = $_POST["fecha_inicio"];
   $f_t = $_POST["fecha_termino"];
 
-  echo "<h1> Reservas del usuario $id entre el $f_i y $f_t </h1>";
+  echo "<h1> Reservas del usuario $id el $f_i y $f_t </h1>";
 
-  $query = "SELECT RE.id_reserva FROM reservas RE, reservas_usuarios RE_U WHERE RE_U.id_usuario = $id AND RE.id_reserva = RE_U.id_reserva AND RE.fecha_inicio != TO_DATE($f_i, 'YYYY-MM-DD') AND RE.fecha_fin != TO_DATE($f_t, 'YYYY-MM-DD');";
+  $query = "SELECT RE.id_reserva FROM reservas RE, reservas_usuarios RE_U WHERE RE_U.id_usuario = $id AND RE.id_reserva = RE_U.id_reserva AND RE.fecha_inicio >= '$f_i' AND RE.fecha_fin <= '$f_t';";
   $result = $db -> prepare($query);
   $result -> execute();
   $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
